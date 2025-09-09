77e37aed177a05a7636a7996ce0b5808b90e8057
Akshat Jain <aj1508109@gmail.com>
2025-09-09 11:26:47 +0530

    Update Sort a Stack.cpp

--- Sort a Stack/Sort a Stack.cpp
+++ Sort a Stack/Sort a Stack.cpp
@@ -1,25 +1,24 @@
-void sortStack(stack<int>& st) {
-    if (!st.empty()) {
-        int top = st.top();
-        st.pop();
+class Solution {
+public:
+    void sortStack(stack<int> &st) {
+        if(!st.empty()){
+            int top=st.top();
+            st.pop();
 
-        // Sort remaining stack
-        sortStack(st);
+            sortStack(st);
 
-        // Insert top element back in sorted order
-        insertInSortedOrder(st, top);
+            insert(st, top);
+        }
     }
-}
 
-void insertInSortedOrder(stack<int>& st, int x) {
-    // If stack is empty OR top element is smaller than x
-    // (since we want descending order, larger elements should be on top)
-    if (st.empty() || st.top() < x) {
-        st.push(x);
-    } else {
-        int temp = st.top();
-        st.pop();
-        insertInSortedOrder(st, x);
-        st.push(temp);
+    void insert(stack<int> &st, int x){
+        if(st.empty()||st.top()<x){
+            st.push(x);
+        }else{
+            int temp=st.top();
+            st.pop();
+            insert(st, x);
+            st.push(temp);
+        }
     }
-}
+};
