d9705b19c4ee235e7cc4802c2faf6e1b32062870
Akshat Jain <aj1508109@gmail.com>
2025-12-30 10:18:03 +0530

    Time: 2 ms (52.81%) | Memory: 10 MB (10.91%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        if(n!=m) return false;
+
+        unordered_map<char,int> mpp1;
+        unordered_map<char,int> mpp2;
+        for(int i=0;i<n;i++){
+            mpp1[s[i]]++;
+            mpp2[t[i]]++;
+        }
+
+        if(mpp1==mpp2){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+};
